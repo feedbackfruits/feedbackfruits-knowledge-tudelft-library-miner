61c778ab69d1109d80a9d1770f142e7b7f216c44
GZ-02 <geo02zarn@gmail.com>
2019-06-05 16:03:49 +0200

    Added LengthExtraction function but still get errors

--- src/index.ts
+++ src/index.ts
@@ -1,93 +1,47 @@
-// Import package that allows meta-data harvesting according to the OAI protocol
-import { OaiPmh } from 'oai-pmh'
+import * as Config from './config';
+import { mine } from './miner';
 
-var i = 0;
-var myArray = ['master thesis', 'bachelor thesis', 'doctoral thesis', 'journal article','review','conference paper','book','book chapter','public lecture'];
-const url = 'http://oai.tudelft.nl/ir';
+import { Miner, Config as _Config } from 'feedbackfruits-knowledge-engine';
 
-function getMetadata(item, index){
-  if ('$' in item.header && item.header['$'] == undefined && item.header['$']['status'] == 'deleted'){
-    console.log("Undefined item", index);
-  }
-  else if (item.metadata == undefined){
-    console.log ("Undefined metadata",index);
-  }
-  else{
-    if ( (item.metadata["oai_dc:dc"]["dc:language"] === "en") ){
-      if (Array.isArray(item.metadata["oai_dc:dc"]["dc:type"])){
-        if ( myArray.includes(item.metadata["oai_dc:dc"]["dc:type"][0]) ){
-          console.log("Item: ", i);
-          (item.metadata["oai_dc:dc"]["dc:title"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:title"]);
-          (item.metadata["oai_dc:dc"]["dc:rights"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:rights"]);
-          if (Array.isArray(item.metadata["oai_dc:dc"]["dc:subject"])){
-            console.log(item.metadata["oai_dc:dc"]["dc:subject"]);
-            console.log((item.metadata["oai_dc:dc"]["dc:subject"]).length);
-          }
-          else{
-            (item.metadata["oai_dc:dc"]["dc:subject"] === undefined)? console.log(null):console.log((item.metadata["oai_dc:dc"]["dc:subject"]));
-          }
-          (item.metadata["oai_dc:dc"]["dc:date"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:date"]);
-          (item.metadata["oai_dc:dc"]["dc:type"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:type"]);
-          (item.metadata["oai_dc:dc"]["dc:creator"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:creator"]);
-          if(Array.isArray(item.metadata["oai_dc:dc"]["dc:identifier"])){
-            console.log(item.metadata["oai_dc:dc"]["dc:identifier"][0]);
-          }
-          else{
-            (item.metadata["oai_dc:dc"]["dc:identifier"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:identifier"]);
-          }
-          (item.metadata["oai_dc:dc"]["dc:language"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:language"]);
-          (item.metadata["oai_dc:dc"]["dc:description"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:description"]);
-          (item.metadata["oai_dc:dc"]["dc:contributor"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:contributor"]);
-          i++;
-        }
-      }
-      else{
-        if (myArray.includes(item.metadata["oai_dc:dc"]["dc:type"]) || item.metadata["oai_dc:dc"]["dc:type"] === undefined){
-          console.log("Item: ", i);
-          (item.metadata["oai_dc:dc"]["dc:title"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:title"]);
-          (item.metadata["oai_dc:dc"]["dc:rights"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:rights"]);
-          if (Array.isArray(item.metadata["oai_dc:dc"]["dc:subject"])){
-            console.log((item.metadata["oai_dc:dc"]["dc:subject"]).length);
-          }
-          else{
-            (item.metadata["oai_dc:dc"]["dc:subject"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:subject"]);
-          }
-          (item.metadata["oai_dc:dc"]["dc:date"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:date"]);
-          (item.metadata["oai_dc:dc"]["dc:type"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:type"]);
-          (item.metadata["oai_dc:dc"]["dc:creator"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:creator"]);
-          if(Array.isArray(item.metadata["oai_dc:dc"]["dc:identifier"])){
-            console.log(item.metadata["oai_dc:dc"]["dc:identifier"][0]);
-          }
-          else{
-            (item.metadata["oai_dc:dc"]["dc:identifier"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:identifier"]);
-          }
-          (item.metadata["oai_dc:dc"]["dc:language"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:language"]);
-          (item.metadata["oai_dc:dc"]["dc:description"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:description"]);
-          (item.metadata["oai_dc:dc"]["dc:contributor"] === undefined)? console.log(null):console.log(item.metadata["oai_dc:dc"]["dc:contributor"]);
-          i++;
-        }
-      }
-    }
-  }
-}
-
-async function main () {
-
-  const oaiPmh = new OaiPmh(url);
-  const identifierIterator = oaiPmh.listRecords({
-    metadataPrefix: 'oai_dc',
+export default async function init({ name }) {
+  const send = await Miner({
+    name,
+    customConfig: Config as typeof _Config.Base
   });
-  for await (const identifier of identifierIterator) {
-    try {
-      getMetadata(identifier, i);
-      //if (i>100) break;
-    }
-    catch(error){
-      console.log("Error: ",error);
-    }
 
-  }
-  console.log(i);
+  console.log('Starting TUDelft Library miner...');
+  const docs = mine();
+
+  let count = 0;
+  await new Promise((resolve, reject) => {
+    docs.subscribe({
+      next: async (doc) => {
+        count++;
+        console.log(`Sending doc number ${count}:`, doc['@id']);
+        const result = await send({ action: 'write', key: doc['@id'], data: doc });
+        return result;
+      },
+      error: (reason) => {
+        console.log('Miner crashed...');
+        console.error(reason);
+        reject(reason);
+      },
+      complete: () => {
+        console.log('Miner completed');
+        resolve();
+      }
+    });
+  });
+
+  console.log(`Mined ${count} docs from TUDelft Library`);
+  return;
 }
 
-main().catch(console.error)
+// Start the server when executed directly
+declare const require: any;
+if (require.main === module) {
+  console.log("Running as script.");
+  init({
+    name: Config.NAME,
+  }).catch(console.error);
+}
